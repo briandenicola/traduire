3c5ee7153bc19ec172c45db795666bd4e1b922c0
briandenicola <brian@briandenicola.com>
2022-02-23 12:54:02 -0600

    Updated pipeline

--- infrastructure/create_infrastructure.ps1
+++ infrastructure/create_infrastructure.ps1
@@ -3,71 +3,12 @@ param(
   [string] $AppName,
 
   [Parameter(Mandatory=$true)]
-  [string] $SubscriptionName,
-
-  [Parameter(Mandatory=$true)]
-  [string] $region,
-
-  [Parameter(Mandatory=$true)]
-  [string] $AdminUser,
-
-  [Parameter(Mandatory=$true)]
-  [string] $AdminID,
-
-  [Parameter(Mandatory=$true)]
   [string] $StorageAccountKey
 )
 
-$today = (Get-Date).ToString("yyyyMMdd")
-
-az account set -s $SubscriptionName
-
-#Azure Environment 
-$tenantId = (az account show --query "tenantId" -o tsv)
-
-#Terraform Variables
-$tfVarFileName = "variables.tfvars"
-$tfPlanFileName = "{0}.plan.{1}" -f $AppName, $today
-
-#Resource Names
-$acrAccountName = "{0}acr01" -f $appName
-$appInsightsName = "{0}-ai01" -f $appName
-$logAnalyticsWorkspace = "{0}-logs01" -f $appName
-$vnetName = "{0}-vnet01" -f $appName 
-$aks = "{0}-aks01" -f $appName
-$mp3StorageAccountName = "{0}files01" -f $appName
-$uiStorageAccountName = "{0}ui01" -f $appName
-$postgresqlAccountName = "{0}-psql01" -f $appName
-$serviceBusAccountName = "{0}-sb01" -f $appName
-$keyVaultAccountName = "{0}-kv01" -f $appName
-$pubsubAccountName = "{0}-pubsub01" -f $appName
-
-$public_ip = (Invoke-RestMethod http://checkip.amazonaws.com/).Trim()
-
-$configuration=@"
-application_name = "$appName"
-region = "$region"
-tenant_id = "$tenantId"
-admin_user_object_id = "$AdminID"
-admin_user_name = "$AdminUser"
-postgresql_name = "$postgresqlAccountName"
-acr_account_name = "$acrAccountName"
-ai_account_name = "$appInsightsName"
-loganalytics_account_name = "$logAnalyticsWorkspace"
-vnet_name = "$vnetName"
-aks_name = "$aks"
-ui_storage_name = "$uiStorageAccountName"
-pubsub_name = "$pubsubAccountName"
-mp3_storage_name = "$mp3StorageAccountName"
-service_bus_namespace_name = "$serviceBusAccountName"
-keyvault_name = "$keyVaultAccountName"
-api_server_authorized_ip_ranges = "$public_ip/32"
-"@
-Set-Content -Value $configuration -Path ./terraform/$tfVarFileName -Encoding ascii
-
 Set-Location ./terraform
-terraform init -backend=true -backend-config="access_key=$StorageAccountKey" -backend-config="key=traduire.terraform.tfstate"
-terraform plan -out="$tfPlanFileName" -var-file="$tfVarFileName"
+terraform init -backend=true -backend-config="access_key=$StorageAccountKey" -backend-config="key=trad8b43xz.terraform.tfstate"
+terraform plan -out="$tfPlanFileName" -var-file="shared-cluster.tfvars"
 terraform apply -auto-approve $tfPlanFileName
 
 # echo Application name
