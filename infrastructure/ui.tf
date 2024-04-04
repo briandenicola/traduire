fc9bd0c49edc00d26a6c07c2c49dffbffb6a0bd2
briandenicola <brian@briandenicola.com>
2024-04-04 12:41:43 -0500

    🎳 Thu Apr  4 12:41:43 CDT 2024

--- infrastructure/ui.tf
+++ infrastructure/ui.tf
@@ -1,7 +1,9 @@
-resource "azurerm_static_site" "traduire_ui" {
+resource "azurerm_static_web_app" "traduire_ui" {
   name                  = local.static_webapp_name
   resource_group_name   = azurerm_resource_group.traduire_ui.name
   location              = "centralus"
+  sku_size = "Free"
+  sku_tier = "Free"
 }
 
 resource "azurerm_web_pubsub" "traduire_app" {
