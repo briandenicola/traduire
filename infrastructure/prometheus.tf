465b29c1ea9cbcbe52743524f2bbc219314a9e03
briandenicola <brian@briandenicola.com>
2024-04-11 11:13:22 -0500

    Added Grafana

--- infrastructure/prometheus.tf
+++ infrastructure/prometheus.tf
@@ -1,18 +1,3 @@
-# resource "azapi_resource" "azure_monitor_workspace" {
-#   type      = "microsoft.monitor/accounts@2021-06-03-preview"
-#   name      = "${local.resource_name}-workspace"
-#   parent_id = azurerm_resource_group.this.id
-
-#   location = azurerm_resource_group.this.location
-
-#   body = jsonencode({
-#   })
-# }
-
-# locals {
-#   am_workspace_id = "${data.azurerm_subscription.current.id}/resourcegroups/${azurerm_resource_group.this.name}/providers/microsoft.monitor/accounts/${local.resource_name}-workspace"
-# }
-
 resource "azurerm_monitor_workspace" "this" {
   name                = "${local.resource_name}-workspace"
   resource_group_name = azurerm_resource_group.traduire_app.name
--- infrastructure/roles.tf
+++ infrastructure/roles.tf
@@ -20,9 +20,9 @@ resource "azurerm_role_assignment" "acr_pullrole_node" {
 }
 
 resource "azurerm_role_assignment" "admin" {
-  scope                = azurerm_key_vault.traduire_app.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id 
+  scope                            = azurerm_key_vault.traduire_app.id
+  role_definition_name             = "Key Vault Administrator"
+  principal_id                     = data.azurerm_client_config.current.object_id 
 }
 
 resource "azurerm_role_assignment" "keda_sb_data_owner" {
@@ -50,5 +50,19 @@ resource "azurerm_role_assignment" "certs" {
   scope                            = azurerm_key_vault.traduire_app.id
   role_definition_name             = "Key Vault Certificates Officer"
   principal_id                     = azurerm_user_assigned_identity.traduire_identity.principal_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "grafana_monitor_data_reader" {
+  scope                            = azurerm_resource_group.traduire_app.id
+  role_definition_name             = "Monitoring Data Readerer"
+  principal_id                     = azurerm_dashboard_grafana.this.identity[0].principal_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "grafana_monitor_read" {
+  scope                            = azurerm_resource_group.traduire_app.id
+  role_definition_name             = "Monitoring Reader"
+  principal_id                     = azurerm_dashboard_grafana.this.identity[0].principal_id
   skip_service_principal_aad_check = true
 }
