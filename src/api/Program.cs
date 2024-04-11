645e918442dbd0cb02055ba3995bf3dc7adbfec0
briandenicola <brian@briandenicola.com>
2024-04-11 10:01:48 -0500

    Add Prometheus scraping

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace traduire.webapi
@@ -28,7 +20,7 @@ namespace traduire.webapi
                 {
                     webBuilder.ConfigureKestrel(options =>
                     {
-                        options.ListenAnyIP(9090);
+                        options.ListenAnyIP(9091);
                         options.ListenAnyIP(8080, o => o.Protocols = HttpProtocols.Http1AndHttp2);
                     });
 
